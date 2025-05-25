a09bc128978b72eb6d5d2233874b3060cfba1f50
Victório <victoriopinto@gmail.com>
2025-05-25 20:45:44 -0300

    1.5 sprint 2

---
+++ codigo/public/assets/js/formulario.js
@@ -0,0 +1,30 @@
+const CHAT_API_URL = "http://localhost:3000/candidatos-ajudante";
+const form = document.getElementById("form-ajudante");
+const usuarioCorrentea = JSON.parse(sessionStorage.getItem("usuarioCorrente"));
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+  data.nome = usuarioCorrentea.nome;
+  data.email = usuarioCorrentea.email;
+
+  try {
+    const response = await fetch(CHAT_API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error("Erro ao enviar os dados do candidato");
+    }
+
+    const result = await response.json();
+    console.log("Candidato enviado com sucesso:", result);
+    alert("Candidato enviado com sucesso!");
+    form.reset(); // Limpa o formulário após o envio
+  } catch (error) {
+    console.error("Erro ao enviar o candidato:", error);
+    alert("Ocorreu um erro ao enviar o candidato. Tente novamente.");
+  }
+});
